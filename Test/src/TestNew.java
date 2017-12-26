0d578434b61c9f85f680f150cde67a2e6aea790a
Siva Kumar <34453556+eanugan@users.noreply.github.com>
2017-12-26 22:45:48 +0530

    Update TestNew.java

--- Test/src/TestNew.java
+++ Test/src/TestNew.java
@@ -2,7 +2,7 @@ public class TestNew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Kumar Siva");
+		System.out.println("Hello Kumar Siva in New Test Class");
 	}
 
 }
